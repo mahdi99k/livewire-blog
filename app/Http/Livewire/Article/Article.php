45ae226936e7adbbc469be718189bee85540c954
mahdi <mahdishmshm13781999@gmail.com>
2021-12-30 18:07:46 +0330

    create store comment

--- app/Http/Livewire/Article/Article.php
+++ app/Http/Livewire/Article/Article.php
@@ -3,23 +3,45 @@
 namespace App\Http\Livewire\Article;
 
 use \App\Models\Article as ModelsArticle;
+use App\Models\Comment;
 use Livewire\Component;
 
 class Article extends Component
 {
 
     public $article;
+    public $comment_text = "";
 
-    public function mount($id)  //Route::get('/article/{id}'
+
+    public function addComment()
+    {
+
+        $this->validate([
+            'comment_text' => 'required|string|max:2000|regex:/^[ا-یa-zA-Z0-9 ? : - . , * !]+$/u'  //حروف فارسی و انگلیسی و اعداد و اشکال
+        ]);
+
+        $comment = new Comment();
+        $comment->text = $this->comment_text;
+        $comment->user_id = \Auth::user()->id;
+        $comment->article_id = $this->article->id;
+        $comment->is_active = 1;
+        $comment->parent_id = 0;
+        $comment->save();
+        $this->comment_text = "";
+//      $this->reset('comment_text');
+    }
+
+
+    public function mount($id)  //Route::get('/article/{id}' | این قسمت فقط بار اول که وارد صفحه میشیم قرار میده تغییرات دفعه بعدی باید رفرش کینم
     {
         $this->article = ModelsArticle::query()->findOrFail($id);
     }
 
 
-
-    public function render()
+    public function render()   //میاد بدون رفرش قرار میده تو صفحه
     {
-        return view('livewire.article.article');
+        $comments = Comment::query()->where('article_id', $this->article->id)->get();  //خود کار بدون رفرش کامنت قرار میگیره
+        return view('livewire.article.article', ['comments' => $comments]);
     }
 
 }
--- resources/views/livewire/article/article.blade.php
+++ resources/views/livewire/article/article.blade.php
@@ -2,6 +2,8 @@
 <main class="main container">
 
     <div class="row my-4">
+
+        {{------------ content ------------}}
         <div id="articleRight" class="col-12 col-md-8 col-xl-9">
             <div class="p-2 bg-light rounded">
                 <h1 class="text-center font_2 py-2">{{ $article->h_title }}</h1>
@@ -13,6 +15,8 @@
 
         </div>
 
+
+        {{------------ sidebar ------------}}
         <div id="articleLeft" class="col-12 col-md-4 col-xl-3 mt-3 mt-md-0">
             <div class="row bg-light px1 py-5 text-center justify-content-center d-flex rounded w-100 m-auto">
                 <div class="image rounded-circle overflow-hidden h_10 w_10 text-center justify-content-center">
@@ -39,6 +43,8 @@
         </div>
     </div>
 
+
+    {{------------ comments ------------}}
     <div class="row justify-content-center align-items-center alert-secondary p-3">
 
         <div class="row p-3 justify-content-center text-right alert-light d-block mb-4 col-12">
@@ -47,25 +53,43 @@
             @endforeach
         </div>
 
-        <div class="col-12 row justify-content-center form-group">
-            <input type="text" class="form-control rounded_5 col-12 col-md-8" placeholder="نام نویسنده نظر">
-        </div>
-        <div class="col-12 row justify-content-center form-group">
-            <h5 class="col-12 text-center">متن نظر:</h5>
-            <textarea rows="10" class="form-control rounded shadow col-12 col-md-8 "></textarea>
-        </div>
+
+        @if (auth()->check())
+
+            <div class="col-12 row justify-content-center form-group">
+                <h5 class="col-12 text-center">متن نظر:</h5>
+                <textarea name="comment_text" rows="10" class="form-control rounded shadow col-12 col-md-8" wire:model="comment_text"></textarea>
+                @error('comment_text')
+                <small class="d-block text-danger w-100 text-center">{{ $message }}</small>
+                @enderror
+
+                <div class="text-center col-12">
+                    <button type="button" class="btn btn-success rounded_5 mt-3 pl-4 pr-4" wire:click="addComment">ثبت نظر</button>
+                </div>
+            </div>
+
+        @else
+
+            <p class="text-primary text-center"><a href="{{ route('login') }}">لطفا جهت ثبت نظر وارد شوید</a></p>
+
+        @endif
+
 
         <div class="col-12 col-md-11 bg-white p-3">
 
-            @forelse ($article->comments as $comment)
+            @forelse ($comments as $comment)  {{-- درون رندر فراخوانی کردیم بدون رفرش دیگه نمیخواد $article->comments --}}
 
                 <div class="row my-2 d-block p-2 rounded shadow-sm border_1 col-11 m-auto shadow">
                     <div class="row justify-content-lg-between w-100 m-auto">
                         <h6 class="text-right text-success"> {{$comment->user->name}} | تاریخ : <span>{{ $comment->created_at->diffForHumans() }}</span> </h6>
+
+                        @if ($comment->user_id === auth()->user()->id)    {{-- فقط پیام های خودم بتونم حذف کنم --}}
                         <span>
-                        <i class="fas fa-trash text-danger cursor_pointer_text_shadow mx-2"></i>
-                        <i class="fas fa-edit text-success cursor_pointer_text_shadow mx-2"></i>
+                            <i class="fas fa-trash text-danger cursor_pointer_text_shadow mx-2"></i>
+                            {{--<i class="fas fa-edit text-success cursor_pointer_text_shadow mx-2"></i>--}}
                         </span>
+                        @endif
+
                     </div>
 
                     <div class=" w-100 pb-3">
